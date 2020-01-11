4b9b854cff328e3d1eb1f99c42fbe049881fc811
BrandonBrown <brandonbrown@pursuit.org>
2020-01-11 17:50:57 -0500

    save

--- app.js
+++ app.js
@@ -9,31 +9,32 @@ const app = express()
  const animalArray = ['Lion','Zebra','Monkey','dinosaur','fish']
  const range = []
  const randomNum = () => {
-     Math.floor(Math.random(2)*animalArray.length)
-    // res.json(randomNum())
- }
-
- const isAnimal = (req, res, next) => {
-     let animal = req.params.id 
+     next()
+     // res.json(randomNum())
+    }
     
-         if(!animalArray.includes(animal)){
-             res.json({
-                 status: "failed",
-                 message: "false"
-             })
-             
-         } else{
+    const isAnimal = (req, res, next) => {
+        let animal = req.params.id 
+        
+        if(!animalArray.includes(animal)){
+            res.json({
+                status: "failed",
+                message: "false"
+            })
+            
+        } else{
             res.json({
                 status: "success",
                 message: "true"
             })
-
-         }
-     
-    next()
-}
-const generateSpread = (req,res,next) => {
-    res.json
+            
+        }
+        
+        next()
+    }
+    const generateSpread = (req,res,next) => {
+        res.json
+        console.log(Math.floor(Math.random(2)*animalArray.length))
     next()
 }
 
@@ -44,8 +45,8 @@ const generateSpread = (req,res,next) => {
      })
  })
 
- app.get("/random"),randomNum,(req,res) => {
-     res.json(randomNum())
+ app.get('/random'), generateSpread,(req,res) => {
+     res.json("sucess: " + randomNum)
      
  }
 
